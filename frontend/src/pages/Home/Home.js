80ce36048d4df966587ef16a2484eda5c8f6fa9e
Singa-pirate <luojiale@u.nus.edu>
2024-09-16 14:52:22 +0800

    init frontend react app

---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import logo from '../../logo.svg';
+import './Home.scss';
+
+function Home() {
+    return (
+        <div className="Home">
+            <header className="Home-header">
+            <img src={logo} className="Home-logo" alt="logo" />
+            <p>
+                Welcome to lucKY JiNX!
+            </p>
+            </header>
+        </div>
+    )
+}
+
+export default Home;
