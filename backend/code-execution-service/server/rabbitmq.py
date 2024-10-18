54aaa81b93ecae6257134256ae80d45788df1f85
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2024-10-18 17:11:25 +0800

    Declare channel only on demand

--- backend/code-execution-service/server/rabbitmq.py
+++ backend/code-execution-service/server/rabbitmq.py
@@ -1,13 +1,14 @@
 import pika
 import os
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-    host=os.environ.get('RABBITMQ_HOST'), port=os.environ.get('RABBITMQ_PORT'), heartbeat=30))
-channel = connection.channel()
-
-channel.queue_declare(queue='code-execution', durable=True)
+def _get_channel():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host=os.environ.get('RABBITMQ_HOST'), port=os.environ.get('RABBITMQ_PORT'), heartbeat=30))
+    channel = connection.channel()
+    channel.queue_declare(queue='code-execution', durable=True)
+    return channel
 
 
 def send_message(message: str) -> None:
-    channel.basic_publish(exchange='', routing_key='code-execution', body=message,
+    _get_channel().basic_publish(exchange='', routing_key='code-execution', body=message,
                           properties=pika.BasicProperties(delivery_mode=pika.DeliveryMode.Persistent))
