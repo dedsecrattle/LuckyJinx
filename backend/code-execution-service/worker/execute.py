6f86348eecc3879a270bd83163e065a26d0d506b
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2024-10-20 22:16:06 +0800

    Dockerise languages in code execution

--- backend/code-execution-service/worker/execute.py
+++ backend/code-execution-service/worker/execute.py
@@ -1,22 +1,31 @@
 import subprocess
+from subprocess import CompletedProcess
 import os
+
+compile_shell = os.environ.get("COMPILE_SHELL") == "True"
 
 def execute(code: str, lang: str, input: str, timeout: int) -> tuple[str | None, str | None]:
     match lang:
         case "python":
-            return execute_python(code, input, timeout)
+            return _execute_python(code, input, timeout)
         case "javascript":
-            return execute_javascript(code, input, timeout)
+            return _execute_javascript(code, input, timeout)
         case "typescript":
-            return execute_typescript(code, input, timeout)
+            return _execute_typescript(code, input, timeout)
         case "java":
-            return execute_java(code, input, timeout)
+            return _execute_java(code, input, timeout)
         case "c":
-            return execute_c(code, input, timeout)
+            return _execute_c(code, input, timeout)
         case "c++":
-            return execute_cpp(code, input, timeout)
+            return _execute_cpp(code, input, timeout)
 
-def execute_python(code: str, input: str, timeout: int) -> tuple[str | None, str | None]:
+def _handle_compile_error(result: CompletedProcess[bytes]) -> tuple[str | None, str | None]:
+    if result.stderr:
+        return None, result.stderr.decode()
+    else:
+        return None, result.stdout.decode()
+
+def _execute_python(code: str, input: str, timeout: int) -> tuple[str | None, str | None]:
     if not os.path.exists("temp"):
         os.mkdir("temp")
     with open("temp/solution.py", "w") as f:
@@ -32,7 +41,7 @@ def execute_python(code: str, input: str, timeout: int) -> tuple[str | None, str
     except subprocess.TimeoutExpired:
         return None, "Timeout"
 
-def execute_javascript(code: str, input: str, timeout: int) -> tuple[str | None, str | None]:
+def _execute_javascript(code: str, input: str, timeout: int) -> tuple[str | None, str | None]:
     if not os.path.exists("temp"):
         os.mkdir("temp")
     with open("temp/solution.js", "w") as f:
@@ -48,7 +57,7 @@ def execute_javascript(code: str, input: str, timeout: int) -> tuple[str | None,
     except subprocess.TimeoutExpired:
         return None, "Timeout"
 
-def execute_typescript(code: str, input: str, timeout: int) -> tuple[str | None, str | None]:
+def _execute_typescript(code: str, input: str, timeout: int) -> tuple[str | None, str | None]:
     if not os.path.exists("temp"):
         os.mkdir("temp")
     with open("temp/solution.ts", "w") as f:
@@ -57,10 +66,10 @@ def execute_typescript(code: str, input: str, timeout: int) -> tuple[str | None,
     try:
         result = subprocess.run(["tsc", "temp/solution.ts"],
                                 capture_output=True,
-                                shell=True,
+                                shell=compile_shell,
                                 )
         if result.returncode != 0:
-            return None, result.stderr.decode()
+            return _handle_compile_error(result)
         result = subprocess.run(["node", "temp/solution.js"],
                                 input=input.encode(),
                                 capture_output=True,
@@ -70,7 +79,7 @@ def execute_typescript(code: str, input: str, timeout: int) -> tuple[str | None,
     except subprocess.TimeoutExpired:
         return None, "Timeout"
 
-def execute_java(code: str, input: str, timeout: int) -> tuple[str | None, str | None]:
+def _execute_java(code: str, input: str, timeout: int) -> tuple[str | None, str | None]:
     if not os.path.exists("temp"):
         os.mkdir("temp")
     with open("temp/Solution.java", "w") as f:
@@ -79,10 +88,10 @@ def execute_java(code: str, input: str, timeout: int) -> tuple[str | None, str |
     try:
         result = subprocess.run(["javac", "temp/Solution.java"],
                                 capture_output=True,
-                                shell=True,
+                                shell=compile_shell,
                                 )
         if result.returncode != 0:
-            return None, result.stderr.decode()
+            return _handle_compile_error(result)
         result = subprocess.run(["java", "-cp", "temp", "Solution"],
                                 input=input.encode(),
                                 capture_output=True,
@@ -92,7 +101,7 @@ def execute_java(code: str, input: str, timeout: int) -> tuple[str | None, str |
     except subprocess.TimeoutExpired:
         return None, "Timeout"
 
-def execute_c(code: str, input: str, timeout: int) -> tuple[str | None, str | None]:
+def _execute_c(code: str, input: str, timeout: int) -> tuple[str | None, str | None]:
     if not os.path.exists("temp"):
         os.mkdir("temp")
     with open("temp/solution.c", "w") as f:
@@ -101,10 +110,10 @@ def execute_c(code: str, input: str, timeout: int) -> tuple[str | None, str | No
     try:
         result = subprocess.run(["gcc", "temp/solution.c", "-o", "temp/solution"],
                                 capture_output=True,
-                                shell=True,
+                                shell=compile_shell,
                                 )
         if result.returncode != 0:
-            return None, result.stderr.decode()
+            return _handle_compile_error(result)
         result = subprocess.run(["temp/solution"],
                                 input=input.encode(),
                                 capture_output=True,
@@ -114,7 +123,7 @@ def execute_c(code: str, input: str, timeout: int) -> tuple[str | None, str | No
     except subprocess.TimeoutExpired:
         return None, "Timeout"
 
-def execute_cpp(code: str, input: str, timeout: int) -> tuple[str | None, str | None]:
+def _execute_cpp(code: str, input: str, timeout: int) -> tuple[str | None, str | None]:
     if not os.path.exists("temp"):
         os.mkdir("temp")
     with open("temp/solution.cpp", "w") as f:
@@ -123,10 +132,10 @@ def execute_cpp(code: str, input: str, timeout: int) -> tuple[str | None, str | 
     try:
         result = subprocess.run(["g++", "temp/solution.cpp", "-o", "temp/solution"],
                                 capture_output=True,
-                                shell=True,
+                                shell=compile_shell,
                                 )
         if result.returncode != 0:
-            return None, result.stderr.decode()
+            return _handle_compile_error(result)
         result = subprocess.run(["temp/solution"],
                                 input=input.encode(),
                                 capture_output=True,
--- backend/code-execution-service/worker/rabbitmq.py
+++ backend/code-execution-service/worker/rabbitmq.py
@@ -1,33 +1,34 @@
 import pika
 import os
 import json
+import logging
 from dotenv import load_dotenv
 
 load_dotenv()
+logging.basicConfig(level=logging.INFO)
 
 from execute import execute
 from redis_model import start_task, finish_task
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-    host=os.environ.get('RABBITMQ_HOST'), port=os.environ.get('RABBITMQ_PORT'), heartbeat=30))
-channel = connection.channel()
-
-channel.queue_declare(queue='code-execution', durable=True)
-
 def callback(ch, method, properties, body):
     data = json.loads(body)
-    print(data)
+    logging.info(f"{data=}")
     start_task(data["id"])
     output, error = execute(data["code"], data["lang"], data["input"], int(data["timeout"]))
     finish_task(data["id"], output, error)
-    print(output, error)
+    logging.info(f"{output=}, {error=}")
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 def main():
+    logging.info("Starting worker")
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+    host=os.environ.get('RABBITMQ_HOST'), port=os.environ.get('RABBITMQ_PORT'), heartbeat=30))
+    channel = connection.channel()
+    channel.queue_declare(queue='code-execution', durable=True)
     channel.basic_qos(prefetch_count=1)
     channel.basic_consume(queue='code-execution',
                           on_message_callback=callback)
-    print('Ready to receive messages')
+    logging.info('Ready to receive messages')
     channel.start_consuming()
 
 
