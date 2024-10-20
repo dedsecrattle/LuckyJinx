c6e910d4b1d165544a382f2a4fe7704c0e0203a6
dedsecrattle <itsprabxxx@gmail.com>
2024-10-21 01:43:54 +0800

    fix docker-compose & bugs

--- backend/matching-service/src/server.ts
+++ backend/matching-service/src/server.ts
@@ -1,30 +1,31 @@
-import express from 'express';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import { setupRabbitMQ } from './rabbitmq';
-import { handleMatchingRequest, handleDisconnected } from './matchingService';
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { setupRabbitMQ } from "./rabbitmq";
+import { handleMatchingRequest, handleDisconnected } from "./matchingService";
 
 const app = express();
+
 const server = createServer(app);
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
-  }
+    origin: "*",
+  },
 });
 
 const PORT = process.env.PORT || 3003;
 
 function setupSocketIO(io: Server) {
-  io.on('connection', (socket) => {
-    console.log('A user connected:', socket.id);
+  io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
 
-    socket.on('matching_request', async (userRequest) => {
-      console.log('Matching request received:', userRequest);
+    socket.on("matching_request", async (userRequest) => {
+      console.log("Matching request received:", userRequest);
       await handleMatchingRequest(userRequest, socket.id);
     });
 
-    socket.on('disconnect', async () => {
-      console.log('User disconnected:', socket.id);
+    socket.on("disconnect", async () => {
+      console.log("User disconnected:", socket.id);
       await handleDisconnected(socket.id);
     });
   });
