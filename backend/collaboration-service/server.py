547a73c9e703a043fb51098abfe7c0a6cf0e0d0a
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2024-10-31 22:31:58 +0800

    Add user authentication for collaboration service

--- backend/collaboration-service/server.py
+++ backend/collaboration-service/server.py
@@ -7,26 +7,41 @@ dotenv.load_dotenv()
 
 from events import Events
 from models import Room, User
+from user_verification import authenticate
 
 logging.basicConfig(level=int(os.environ.get('LOG_LEVEL', logging.INFO)))
 
 sio = socketio.AsyncServer(cors_allowed_origins='*', async_mode='asgi')
 app = socketio.ASGIApp(sio)
+unauthenticated_sids = set()
 
 
 @sio.event
 async def connect(sid, environ):
     logging.info(f'connect {sid=}')
+    for header in environ['asgi.scope']['headers']:
+        if header[0].lower() == b'authorization':
+            token = header[1].decode()
+            logging.debug(f"connect {token=}")
+            username = authenticate(token)
+            if username:
+                User(username, sid)
+            else:
+                unauthenticated_sids.add(sid)
+                await sio.disconnect(sid)
+            return
+    unauthenticated_sids.add(sid)
+    await sio.disconnect(sid)
 
 
 @sio.on(Events.JOIN_REQUEST)
 async def join_request(sid, data):
     logging.debug(f'join_request {sid=} {data=}')
     room_id = data['room_id']
-    username = data['username']
+    user: User = User.users[sid]
     await sio.enter_room(sid, room_id)
-    room: Room = Room.get_or_create(room_id, User(username, sid))
-    await sio.emit(Events.JOIN_REQUEST, room.details(), room=room_id, skip_sid=sid)
+    room: Room = Room.get_or_create(room_id, user)
+    await sio.emit(Events.JOIN_REQUEST, room.details(), room=room_id)
 
 
 @sio.on(Events.CODE_UPDATED)
@@ -50,6 +65,9 @@ async def cursor_updated(sid, data):
 @sio.event
 async def disconnect(sid):
     logging.info(f'disconnect {sid=}')
+    if sid in unauthenticated_sids:
+        unauthenticated_sids.remove(sid)
+        return
     user: User = User.users[sid]
     room: Room = user.room
     room_still_exists = room.remove_user(user)
---
+++ backend/collaboration-service/user_verification.py
@@ -0,0 +1,12 @@
+import requests
+import os
+
+USER_SERVICE_URL = os.environ.get('USER_SERVICE_URL')
+if not USER_SERVICE_URL:
+    raise ValueError('USER_SERVICE_URL environment variable not set')
+
+def authenticate(authorization_header) -> str | None:
+    response = requests.get(f"{USER_SERVICE_URL}/auth/verify-token", headers={'Authorization': authorization_header})
+    if response.status_code != 200:
+        return None
+    return response.json()["data"]['username']
