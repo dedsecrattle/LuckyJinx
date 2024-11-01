436a2999f31a3dfe8f984d7638eebbb637075bd4
Prabhat Kumar <itsprabxxx@gmail.com>
2024-11-01 23:26:20 +0800

    Finish Implementation

--- backend/matching-service/src/util.ts
+++ backend/matching-service/src/util.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
+import { configDotenv } from "dotenv";
+
+configDotenv();
 const fetchRandomQuestion = async (complexity: string, categories: string) => {
-  const response = await axios.post("http://question:3002/random", {
+  const response = await axios.post(`${process.env.QUESTION_SERVICE}/random`, {
     complexity,
     categories,
   });
